6d76eb7ca4bb9ee73307f7438d5fa29ac0cd4ef8
xdkeun <dragonkeun7@gmail.com>
2023-08-25 16:37:21 +0900

    feat: 소환사명을 통한 검색으로 티어, 승리, 패배 조회 기능 구현, 검색 결과 페이지 디자인 예정

--- src/components/Find.jsx
+++ src/components/Find.jsx
@@ -1,20 +1,29 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import Header from "./Header";
 
 function Find() {
   const { param } = useParams();
   const API_KEY = process.env.REACT_APP_API_KEY;
+  const [id, setId] = useState("");
+  const [accountId, setAccountId] = useState("");
   const [puuid, setPuuid] = useState("");
   const [nickname, setNickname] = useState("");
-
+  const [tier, setTier] = useState("");
+  const [rank, setRank] = useState("");
+  const [name, setName] = useState("");
+  const [wins, setWins] = useState("");
+  const [losses, setLosses] = useState("");
   useEffect(() => {
-    // 첫 번째 API 호출
+    //summonerv4Api(id, accountId, puuid)
     axios
       .get(
         `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${param}?api_key=${API_KEY}`
       )
       .then(function (response) {
+        setId(response.data.id);
+        setAccountId(response.data.accountId);
         setPuuid(response.data.puuid);
       })
       .catch(function (error) {
@@ -25,25 +34,35 @@ function Find() {
   useEffect(() => {
     // puuid 값이 변경될 때마다 실행될 함수
     if (puuid) {
-      finalSearch();
+      leaguev4Api();
     }
   }, [puuid]);
 
-  function finalSearch() {
-    // 두 번째 API 호출
+  //티어, 소환사명, 승리, 패배
+  function leaguev4Api() {
     axios
       .get(
-        `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`
+        `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${API_KEY}`
       )
       .then(function (response) {
-        setNickname(response.data.name);
+        setTier(response.data[0].tier);
+        setRank(response.data[0].rank);
+        setName(response.data[0].summonerName);
+        setWins(response.data[0].wins);
+        setLosses(response.data[0].losses);
       })
       .catch(function (error) {
-        alert(error);
+        console.log(error);
       });
   }
 
-  return <div>{nickname}</div>;
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{tier} {rank}</p>
+      <p>{wins}승 {losses}패</p>
+    </div>
+  );
 }
 
 export default Find;
